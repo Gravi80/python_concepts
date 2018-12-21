2b618c6da2d08424edac093a03e22361304e908b
Ravi Sharma <ravis4376@gmail.com>
2018-12-21 21:29:52 +0530

    dataclass

--- data_classes.py
+++ data_classes.py
@@ -2,7 +2,6 @@ from dataclasses import dataclass, field, fields
 from datetime import datetime
 from pprint import pprint
 
-
 # https://www.python.org/dev/peps/pep-0557/
 # https://docs.python.org/3/library/dataclasses.html
 
@@ -11,6 +10,9 @@ from pprint import pprint
 #  eq   => __eq__() method will be generated
 # order =>  __lt__(), __le__(), __gt__(), and __ge__() methods will be generated
 # unsafe_hash = False => a __hash__() method is generated
+from typing import List
+
+
 @dataclass(init=True, repr=True, eq=True, order=False, unsafe_hash=False, frozen=False)
 class Account:
     id: int
@@ -49,6 +51,42 @@ class Employee:
 e1 = Employee(emp_id='123', name='Emp1', gender='Female', salary=10, age=10)
 e2 = Employee(emp_id='124', name='Emp2', gender='Male', salary=5, age=20)
 
-pprint(sorted([e1,e2]))
+pprint(sorted([e1, e2]))
 
 fields(e1)
+
+
+@dataclass
+class InventoryItem:
+    name: str
+    unit_price: float
+    total_cost: float = field(
+        init=False)  # Don't expect this field at instantiation time, expect it to be created later
+    quantity: int = 0
+    related_items: List = field(default_factory=list)
+
+    def __post_init__(self):
+        self.total_cost = self.unit_price * self.quantity
+
+
+item = InventoryItem("book", 11, 10)
+print(item.total_cost)
+
+
+# field to be computed at instantiation time but should not be modifiable afterword
+@dataclass(frozen=True)
+class InventoryItem:
+    name: str
+    unit_price: float
+    total_cost: float = field(
+        init=False)  # Don't expect this field at instantiation time, expect it to be created later
+    quantity: int = 0
+    related_items: List = field(default_factory=list)
+
+    def __post_init__(self):
+        total_cost = self.unit_price * self.quantity
+        object.__setattr__(self, 'total_cost', total_cost)
+
+
+item = InventoryItem("book", 11, 10)
+print(item.total_cost)

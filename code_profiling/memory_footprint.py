0a905e41fb8e986e0cd9ddf7cc7531f4ab533af3
Ravi Sharma <ravis4376@gmail.com>
2020-08-16 01:17:49 +0200

    code profiling

--- code_profiling/memory_footprint.py
+++ code_profiling/memory_footprint.py
@@ -1,10 +1,17 @@
-import psutil, os
+import os
+import psutil
+import sys
 
 
 def memory_footprint():
     """Returns memory (in MB) used py python process"""
     mem = psutil.Process(os.getpid()).memory_info().rss
+    # bytes to MB
     return mem / 1024 ** 2
 
 
-print(memory_footprint())
+print(f"before={memory_footprint()}")
+N = (1024 ** 2) // sys.getsizeof(float())  # Number of floats that fill 1 MB
+x = [float(i) for i in range(0, 50 * N)]
+
+print(f"after={memory_footprint()}")

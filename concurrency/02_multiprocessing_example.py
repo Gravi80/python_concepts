fcd274f496173083d29a7ac451dae4988c2d4532
Ravi Sharma <ravis4376@gmail.com>
2022-11-23 21:31:45 +0100

    Issue with multi-processing

--- concurrency/02_multiprocessing_example.py
+++ concurrency/02_multiprocessing_example.py
@@ -4,6 +4,8 @@
 # The Pool class represents a pools of workers processes,
 # we can automatically distribute the tasks among the workers by using the Pool object.
 # The creation of processes and orchestration of tasks will be taken care by Pool.
+import os
+import random
 from multiprocessing import Pool
 
 import requests
@@ -36,6 +38,41 @@ def main():
         with requests.Session() as session:
             pool.starmap(fetch, [(session, URL) for _ in range(100)])
 
+
 # Overhead of creating new processes and also a portion of work
 # is still happening sequentially.
 # In each process the requests processing will happen sequentially.
+
+# ********* Multiprocessing gets stuck when one of the child process is killed ********
+
+
+def some_processing(*args):
+    print(f"{os.getpid()}=start processing")
+    if random.randint(0, 10) % 2 == 0:
+        print(f"Kill {os.getpid()}")
+        # os.kill(os.getpid(), 9)
+        # exit()
+        quit()
+    else:
+        print(f"{os.getpid()}=finish processing")
+    return "Hello"
+
+
+result_list = []
+
+
+def log_result(result):
+    # This is called whenever some_processing() returns a result.
+    # result_list is modified only by the main process, not the pool workers.
+    result_list.append(result)
+
+
+if __name__ == '__main__':
+    pool = Pool(processes=4)
+    # [pool.apply_async(some_processing, (), callback=log_result) for i in range(1)]
+    # [pool.apply(some_processing, ()) for i in range(1)]
+    # pool.map(some_processing, range(1))
+    pool.close()
+    pool.join()
+    # print([f"*****{os.getpid()}={res.get()}" for res in multiple_results])
+    print([f"*****{os.getpid()}={result_list}"])

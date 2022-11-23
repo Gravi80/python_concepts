d42146cd12cda148c2cb22e6944c301258f75af6
Ravi Sharma <ravis4376@gmail.com>
2022-11-23 21:45:27 +0100

    Conditional side-effect

--- testing/pytest_mock/mock_1.py
+++ testing/pytest_mock/mock_1.py
@@ -1,6 +1,7 @@
 # The main characteristic of a Mock object is that it will return another Mock instance when:
 # accessing one of its attributes
 # calling the object itself
+from functools import partial
 
 from mock import Mock, call
 
@@ -32,6 +33,23 @@ def test_side_effect():
     assert m() == 'foo'
     assert m() == 'bar'
     assert m() == 'baz'
+
+
+def _mock_some_url_response(actual_value, different_response_for_input):
+    if actual_value == different_response_for_input:
+        return "different result"
+    else:
+        return "same input"
+
+
+def test_conditional_side_effect():
+    mocked_function = Mock()
+    different_response_for_input = 3
+    mocked_function.side_effect = partial(_mock_some_url_response,
+                                          different_response_for_input=different_response_for_input)
+    assert mocked_function(1) == 'same input'
+    assert mocked_function(2) == 'same input'
+    assert mocked_function(3) == 'different result'
 
 
 def test_raise_exception():
